8b4bbaf3be1c9e329fe6f48f2f6161350f5713a0
CodingIsaac <isaacijuo@gmail.com>
2022-11-25 11:28:51 +0000

    Destructuring Argument Further Commit

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,22 +2,24 @@ import React, { Component, Fragment } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
- 
   render() {
+    const { onReset, counters, onDelete, onIncrement, onDecrement } =
+      this.props;
+
     return (
       <Fragment>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset Counters
         </button>
-        {this.props.counters.map((counter) => (
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           >
             <h2>Open Board</h2>
